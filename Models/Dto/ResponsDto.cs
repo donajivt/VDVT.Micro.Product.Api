3f08ea773de0202632dec60122f13add51d99c1b
“donajivt” <vaniadonajitorres@gmail.com>
2025-05-20 16:47:46 -0600

    Dto del modelo de la respuesta

---
+++ Models/Dto/ResponsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VDVT.Micro.Product.Api.Models.Dto
+{
+    public class ResponsDto
+    {
+        public object Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
